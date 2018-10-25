c01e9569d1a48ca318d8201aac4b0b612617264b
NguyenDanhVuong <vuongndd00547@fpt.edu.vn>
2018-10-25 21:41:48 +0700

    Example

---
+++ Example1/Example1/Views/BlankPage1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Example1.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+
+        private SaveFile SaveFile;
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            this.SaveFile = new SaveFile();
+        }
+
+        private async System.Threading.Tasks.Task SaveFileButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            this.SaveFile.fileName = this.NameFile.Text;
+            this.SaveFile.content = this.Content.Text;
+
+            Windows.Storage.StorageFolder storageFolder =
+            Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile =
+            await storageFolder.CreateFileAsync("file.txt",
+            Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+        }
+    }
+}
---
+++ Example1/Example1/Views/SaveFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Example1.Views
+{
+    class SaveFile
+    {
+        private String _fileName;
+        private String _content;
+
+        public string fileName { get => _fileName; set => _fileName = value; }
+        public string content { get => _content; set => _content = value; }
+    }
+}
