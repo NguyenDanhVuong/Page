6913d8d35b04f5c5bd9de0772bcc7147d8e5af45
NguyenDanhVuong <vuongndd00547@fpt.edu.vn>
2018-10-25 20:06:36 +0700

    KT

---
+++ WindowForms/WindowForms/Page/Page1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowForms.Page
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        private object savePicker;
+
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+        private async System.Threading.Tasks.Task SaveFileButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until
+                // we finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.DeferUpdates(file);
+                // write to file
+                await Windows.Storage.FileIO.WriteTextAsync(file, file.Name);
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                Windows.Storage.Provider.FileUpdateStatus status =
+                    await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    this.FileName.Text = "File " + file.Name + " was saved.";
+                }
+                else
+                {
+                    this.Content.Text = "File " + file.Name + " couldn't be saved.";
+                }
+            }
+            else
+            {
+                this.FileName.Text = "Operation cancelled.";
+            }
+        }
+    }
+}
